e376413a921b58cd9746672fefb47657e2d48089
alec <kaltschmidalec@gmail.com>
2021-07-19 15:28:09 -0700

    commit tree 27709f8efd0a74ff37fa5ab01e835da8d74e585c

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const rideLength = '500'
+
+function scuberGreetingForFeet(rideLength){
+  let ride;
+  if (rideLength <= 400) {
+    ride = "This one is on me!"
+  } else if (rideLength <= 2500) {
+    ride = "I will gladly take your thirty bucks."
+  } else {
+    ride = "No can do."
+  }
+  return ride
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  const isNy = cityName == 'NYC' ? "Ok, sounds good." : "No go."
+  return isNy
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  let tipResponse;
+
+  switch (tipAmount) {
+    case "generous":
+      tipResponse = "Thank you so much."
+      break;
+    case "not as generous":
+      tipResponse = "Thank you."
+      break;
+    default:
+      tipResponse = "Bye."
+  }
+  return tipResponse
 }
